76674d335b4215b6bcbe66f7996e3547d74e1fbd
albero94 <ealbero94@gmail.com>
2020-03-14 17:03:38 -0400

    feat: WebApp - Add Person class that holds name and images for faces

---
+++ FaceRecognitionServer/FaceRecognitionServer/Models/MyPerson.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FaceRecognitionServer.Models
+{
+    public class MyPerson
+    {
+        public string Name { get; set; }
+        public IList<IFormFile> FormFiles { get; set; }
+        public IList<Stream> Images { get; set; }
+
+        public void ReadFormFiles()
+        {
+            foreach(var file in FormFiles)
+            {
+                Images.Add(file.OpenReadStream());
+            }
+        }
+    }
+}
