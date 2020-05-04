e35a501f8c998913a4322ce06bfa7fca82671fca
Sam Frey <samfrey99@gwu.edu>
2020-05-03 23:32:18 -0400

    able to add users from mobile application

---
+++ android/app/src/main/java/com/example/facematchdoorlock/AddUserActivity.kt
@@ -0,0 +1,95 @@
+// Credit for assistance here:
+// https://medium.com/better-programming/how-to-upload-an-image-file-to-your-server-using-volley-in-kotlin-a-step-by-step-tutorial-23f3c0603ec2
+
+package com.example.facematchdoorlock
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.webkit.MimeTypeMap
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.MultipartBody
+import okhttp3.Request
+import java.io.File
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.jetbrains.anko.doAsync
+
+
+class AddUserActivity : AppCompatActivity() {
+
+    lateinit var name: EditText
+    lateinit var imageView: ImageView
+    lateinit var selectImage: Button
+    lateinit var goButton: Button
+    var image: ByteArray? = null
+    var uri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_user)
+
+        name = findViewById(R.id.name)
+        imageView = findViewById(R.id.imageView)
+        selectImage = findViewById(R.id.selectImageButton)
+        goButton = findViewById(R.id.goButton)
+
+        selectImage.setOnClickListener{
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 999)
+        }
+
+        goButton.setOnClickListener{
+            val manager = APImanager()
+            val imgFile = File(uri!!.path)
+            val imgExt = MimeTypeMap.getFileExtensionFromUrl(imgFile.absolutePath)
+            val requestBody = MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("name", name.text.toString())
+                .addFormDataPart("FormFiles", "image", body = image!!.toRequestBody("image/jpg".toMediaType()))
+                .build()
+            val request = Request.Builder()
+                .header("ApiKey", "MySecretKey")
+                .url("https://gw-iot-facerecognitionserver.azurewebsites.net/facerecognition/CreatePerson")
+                .post(requestBody)
+                .build()
+            doAsync {
+                val response = manager.okHttpClient.newCall(request).execute()
+                runOnUiThread {
+                    if (response.isSuccessful) {
+                        Toast.makeText(
+                            this@AddUserActivity,
+                            "Added User",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == 999) {
+            uri = data?.data
+            if (uri != null) {
+                imageView.setImageURI(uri)
+                createImageData(uri!!)
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun createImageData(uri: Uri) {
+        val inputStream = contentResolver.openInputStream(uri)
+        inputStream?.buffered()?.use {
+            image = it.readBytes()
+        }
+    }
+}
--- android/app/src/main/java/com/example/facematchdoorlock/MainActivity.kt
+++ android/app/src/main/java/com/example/facematchdoorlock/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.facematchdoorlock
 
 import android.content.Intent
-import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.Toast
 import okhttp3.Request
 import org.jetbrains.anko.doAsync
@@ -17,7 +15,11 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var checkButton: Button
 
-    lateinit var image: ImageView
+    lateinit var addUserButton: Button
+
+    lateinit var deleteUserButton: Button
+
+    lateinit var listUserButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +27,14 @@ class MainActivity : AppCompatActivity() {
 
         unlockButton = findViewById(R.id.unlockButton)
         checkButton = findViewById(R.id.checkButton)
-        image = findViewById(R.id.imageView)
+        addUserButton = findViewById(R.id.addUserButton)
+        deleteUserButton = findViewById(R.id.deleteUserButton)
+        listUserButton = findViewById(R.id.listUserButton)
         val manager = APImanager()
 
         unlockButton.setOnClickListener { view: View ->
             val request = Request.Builder()
+                .header("ApiKey", "MySecretKey")
                 .url("http://192.168.86.41:5002/unlock")
                 .build()
             doAsync {
@@ -45,19 +50,23 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        /*checkButton.setOnClickListener { view: View ->
-            val request = Request.Builder()
-                .url("http://192.168.86.41:5002/checkdoor")
-                .build()
-            doAsync {
-                val response = manager.okHttpClient.newCall(request).execute()
-                val bitmap = BitmapFactory.decodeStream(response.body?.byteStream())
-                runOnUiThread { image.setImageBitmap(bitmap) }
-            }
-        }*/
-
         checkButton.setOnClickListener { view: View ->
             val intent = Intent(this@MainActivity, CheckDoorActivity::class.java)
+            startActivity(intent)
+        }
+
+        addUserButton.setOnClickListener { view: View ->
+            val intent = Intent(this@MainActivity, AddUserActivity::class.java)
+            startActivity(intent)
+        }
+
+        deleteUserButton.setOnClickListener { view: View ->
+            val intent = Intent(this@MainActivity, DeleteUserActivity::class.java)
+            startActivity(intent)
+        }
+
+        listUserButton.setOnClickListener { view: View ->
+            val intent = Intent(this@MainActivity, ListUserActivity::class.java)
             startActivity(intent)
         }
     }
